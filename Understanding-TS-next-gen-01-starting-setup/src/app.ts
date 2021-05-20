f4d20b7a8c0015e8c54a688897cdb1bb6a43dd43
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-05-20 23:56:58 +0900

    Fix

--- Understanding-TS-next-gen-01-starting-setup/src/app.ts
+++ Understanding-TS-next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,62 @@
 // Code goes here!
+// const add = (a: number, b: number = 1) => {
+//   let result;
+//   result = a + b;
+//   return result;
+// };
+
+// console.log(add(2));
+
+// const printOutput: (output: string | number) => void = (output) => {
+//   console.log(output);
+// };
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', (event) => {
+    console.log(event);
+  });
+}
+
+const hobbies = ['Sportts', 'Cooking'];
+const obj1 = { name: 'hoge', age: 1 };
+const obj2 = { name: 'mac', age: 2 };
+const newObj = { ...obj1, ...obj2 };
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+
+console.log(activeHobbies);
+console.log(newObj);
+
+const add = (...numbers: number[]) => {
+  numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addNumbers = add(5, 1, 2, 3.7);
+console.log(addNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1);
+
+const person = {
+  firstName: 'Max',
+  age: 30,
+  description: 'Hello World!!',
+};
+
+const { firstName, age, ...remainingPram } = person;
+
+console.log(firstName, age, remainingPram);
+
+function test(a: number, b: number) {
+  var result = a + b;
+  return result;
+}
+
+if(age >= 20) {
+  var isAdult = true;
+}
+console.log(isAdult)
