378d35433d8043cc39d109f57af25210aff46856
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-05-21 21:01:34 +0900

    Practice Type Guard

---
+++ Understanding-TS-adv-types-01-starting-setup/src/app.ts
@@ -0,0 +1,78 @@
+// Code goes
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: 'Max',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
+
+// Operator '+' cannot be applied to types 'number' and 'Combinable' が出るため一旦コメントアウト
+// function add(a: Combinable, b: Combinable) {
+//   if (typeof a === 'string' || b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfomation(emp: UnknownEmployee) {
+  console.log('privileges' in emp);
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+    return;
+  }
+  console.log('Not find Privileges property');
+}
+
+const e2: ElevatedEmployee = {
+  name: 'nick',
+  privileges: ['create-server'],
+  startDate: new Date(),
+};
+
+printEmployeeInfomation(e2);
+
+class Car {
+  drive() {
+    console.log('Driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("I'm driving a truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`We're loading up... ${amount}Kg`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) vehicle.loadCargo(1000);
+}
+
+useVehicle(v1);
+useVehicle(v2);
