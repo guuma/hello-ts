0a3f2b24d27b24e42b5b397b3f29c6f029b88036
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-05-22 15:10:00 +0900

    Practice function overload

--- Understanding-TS-adv-types-01-starting-setup/src/app.ts
+++ Understanding-TS-adv-types-01-starting-setup/src/app.ts
@@ -21,112 +21,118 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
-// Operator '+' cannot be applied to types 'number' and 'Combinable' が出るため一旦コメントアウト
-// function add(a: Combinable, b: Combinable) {
-//   if (typeof a === 'string' || b === 'string') {
-//     return a.toString() + b.toString();
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable): Combinable {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add('Hello', 'TypeScript');
+console.log(result.split(""));
+
+// type UnknownEmployee = Employee | Admin;
+
+// function printEmployeeInfomation(emp: UnknownEmployee) {
+//   console.log('privileges' in emp);
+//   if ('privileges' in emp) {
+//     console.log('Privileges: ' + emp.privileges);
+//     return;
 //   }
-//   return a + b;
+//   console.log('Not find Privileges property');
 // }
 
-type UnknownEmployee = Employee | Admin;
+// const e2: ElevatedEmployee = {
+//   name: 'nick',
+//   privileges: ['create-server'],
+//   startDate: new Date(),
+// };
 
-function printEmployeeInfomation(emp: UnknownEmployee) {
-  console.log('privileges' in emp);
-  if ('privileges' in emp) {
-    console.log('Privileges: ' + emp.privileges);
-    return;
-  }
-  console.log('Not find Privileges property');
-}
+// printEmployeeInfomation(e2);
 
-const e2: ElevatedEmployee = {
-  name: 'nick',
-  privileges: ['create-server'],
-  startDate: new Date(),
-};
-
-printEmployeeInfomation(e2);
-
-class Car {
-  drive() {
-    console.log('Driving...');
-  }
-}
-
-class Truck {
-  drive() {
-    console.log("I'm driving a truck");
-  }
-
-  loadCargo(amount: number) {
-    console.log(`We're loading up... ${amount}Kg`);
-  }
-}
-
-type Vehicle = Car | Truck;
-
-const v1 = new Car();
-const v2 = new Truck();
-
-function useVehicle(vehicle: Vehicle) {
-  vehicle.drive();
-  if (vehicle instanceof Truck) vehicle.loadCargo(1000);
-}
-
-useVehicle(v1);
-useVehicle(v2);
-
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: 'horse';
-  runningSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimal(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case 'bird':
-      speed = animal.flyingSpeed;
-      break;
-    case 'horse':
-      speed = animal.runningSpeed;
-  }
-  console.log(`Moving Speed ${speed}`);
-}
-
-const birdObj = {
-  type: 'bird',
-  flyingSpeed: 5,
-};
-
-const paragraph = document.getElementById('message-output');
-console.log(paragraph, typeof paragraph);
-
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
-// const userInputElement = document.getElementById('user-input');
-
-// function setInputValue(userInputElement: HTMLInputElement) {
-//   if (userInputElement) {
-//     (userInputElement as HTMLInputElement).value = 'Hello World';
-//   } else {
-//     console.log('Can not find  userInputElement');
+// class Car {
+//   drive() {
+//     console.log('Driving...');
 //   }
 // }
 
-// setInputValue(userInputElement);
+// class Truck {
+//   drive() {
+//     console.log("I'm driving a truck");
+//   }
 
-interface ErrorContainer {
-  [prop: string]: string;
-}
+//   loadCargo(amount: number) {
+//     console.log(`We're loading up... ${amount}Kg`);
+//   }
+// }
 
-const errorBag: ErrorContainer = {
-  email: 'Please enter the correct error message',
-  name: 'User-name cannot contain symbols'
-};
+// type Vehicle = Car | Truck;
+
+// const v1 = new Car();
+// const v2 = new Truck();
+
+// function useVehicle(vehicle: Vehicle) {
+//   vehicle.drive();
+//   if (vehicle instanceof Truck) vehicle.loadCargo(1000);
+// }
+
+// useVehicle(v1);
+// useVehicle(v2);
+
+// interface Bird {
+//   type: 'bird';
+//   flyingSpeed: number;
+// }
+
+// interface Horse {
+//   type: 'horse';
+//   runningSpeed: number;
+// }
+
+// type Animal = Bird | Horse;
+
+// function moveAnimal(animal: Animal) {
+//   let speed;
+//   switch (animal.type) {
+//     case 'bird':
+//       speed = animal.flyingSpeed;
+//       break;
+//     case 'horse':
+//       speed = animal.runningSpeed;
+//   }
+//   console.log(`Moving Speed ${speed}`);
+// }
+
+// const birdObj = {
+//   type: 'bird',
+//   flyingSpeed: 5,
+// };
+
+// const paragraph = document.getElementById('message-output');
+// console.log(paragraph, typeof paragraph);
+
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// // const userInputElement = document.getElementById('user-input');
+
+// // function setInputValue(userInputElement: HTMLInputElement) {
+// //   if (userInputElement) {
+// //     (userInputElement as HTMLInputElement).value = 'Hello World';
+// //   } else {
+// //     console.log('Can not find  userInputElement');
+// //   }
+// // }
+
+// // setInputValue(userInputElement);
+
+// interface ErrorContainer {
+//   [prop: string]: string;
+// }
+
+// const errorBag: ErrorContainer = {
+//   email: 'Please enter the correct error message',
+//   name: 'User-name cannot contain symbols'
+// };
