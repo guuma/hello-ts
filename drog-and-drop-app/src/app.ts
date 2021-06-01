51d86f5e00da692949ec2011aba4a366384ce8a9
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2021-06-01 22:20:09 +0900

    Modified src/app.ts

--- drog-and-drop-app/src/app.ts
+++ drog-and-drop-app/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -9,7 +12,24 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title');
+    this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description');
+    this.mandayInputElement = <HTMLInputElement>this.element.querySelector('#manday');
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault;
+    console.log(this.titleInputElement.value);
+    console.log(event);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
